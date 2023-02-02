2a724af4f987e72daada115f034139e3463d312e
Andriy-Lik <alan321987@gmail.com>
2023-02-02 20:20:48 +0200

    add todo

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,61 @@ import ColorPicker from './components/ColorPicker';
 import TodoList from './components/TodoList';
 import Form from './components/Form';
 import Container from './components/Container';
+import TodoEditor from './components/TodoEditor';
 
 import initialTodos from './components/Data/todos.json';
 import colorPickerOptions from './components/Data/ColorPicker.json';
-
-
+import shortid from 'shortid';
 
 class App extends Component {
   state = {
     todos: initialTodos
   };
 
+  addTodo = text => {
+    
+    const todo = {
+      id: shortid.generate(),
+      text,
+      completed: false,
+    }
+
+    this.setState(prevState => ({
+      todos: [todo, ...prevState.todos],
+    }));
+  };
+
   deleteTodo = todoId => {
     this.setState(prevState => ({
       todos: prevState.todos.filter(todo => todo.id !== todoId),
     }));
+  };
+
+  toggleCompleted = todoId => {
+    console.log(todoId);
+    this.setState(prevState => ({
+      todos: prevState.todos.map(todo => {
+        if (todo.id === todoId) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      }),
+    }));
+  };
+
+  onMakeTodoClassName = todoId => {
+    const todoClasses = ['TodoList__item'];
+    this.setState(prevState => ({
+        todos: prevState.todos.map(todo => {
+            if (todo.id === todoId) {
+                todoClasses.push('TodoList__item--completed');
+            }
+            return todoClasses.join(' ');
+        })
+    }))
   };
 
   formSubmitHandler = data => {
@@ -45,11 +85,18 @@ class App extends Component {
         <Dropdown />
         <ColorPicker options={colorPickerOptions} />
 
+
         <div style={{marginLeft: "40px"}}>
           <p>Общее кол-во: {totalTodoCount}</p>
           <p className='titleTodoList'>Кол-во выполненных: {completedTodosCount}</p>
         </div>
-        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
+        <TodoEditor onSubmit={this.addTodo} />
+        <TodoList 
+          todos={todos} 
+          onDeleteTodo={this.deleteTodo} 
+          onToggleCompleted={this.toggleCompleted}
+          makeTodoClassName={this.onMakeTodoClassName} 
+        />
       </Container>
     );
   }
